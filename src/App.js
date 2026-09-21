faef8c71e0cbfcc8c02bebe1122b344d10699e19
Keerthi-bca <keerthu9903@gmail.com>
2026-09-21 06:01:07 +0000

    build code

--- src/App.js
+++ src/App.js
@@ -1,46 +1,46 @@
 import React from "react";
-import {BrowserRouter,Routes,Route,Link} from "react-router-dom";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import Home from "./Home";
 import About from "./About";
-import Contact from "./Contact"
+import Contact from "./Contact";
 
 function App() {
-
   return (
-    <div>
+    // 1. Move BrowserRouter to the very top of your return statement
+    <BrowserRouter>
+      <div>
+        <header>
+          <h1>My React Website</h1>
+          {/* Note: changed 'class' to 'className' for proper React JSX */}
+          <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <div className="collapse navbar-collapse" id="navbarNavDropdown">
+              <ul className="navbar-nav">
+                <li className="nav-item active">
+                  <Link className="nav-link" to="/">
+                    Home <span className="sr-only">(current)</span>
+                  </Link>
+                </li>
+                <li className="nav-item">
+                  <Link className="nav-link" to="/about">About Us</Link>
+                </li>
+                <li className="nav-item">
+                  <Link className="nav-link" to="/contact">Contact Us</Link>
+                </li>
+              </ul>
+            </div>
+          </nav>
+        </header>
 
-      <header>
-        <h1>My React Website</h1>
-         <nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <div className="collapse navbar-collapse" id="navbarNavDropdown">
-    <ul className="navbar-nav">
-      <li className="nav-item active">
-        <Link className="nav-link" to="/">Home <span className="sr-only">(current)</span></Link>
-      </li>
-      <li className="nav-item">
-        <Link className="nav-link" to="/about">About Us</Link>
-      </li>
-      <li className="nav-item">
-        <Link className="nav-link" to="/contact">Contact Us</Link>
-      </li>
-      </ul>
+        <main>
+          {/* 2. Removed BrowserRouter from here so it only wraps Routes */}
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/contact" element={<Contact />} />
+          </Routes>
+        </main>
       </div>
-        </nav>
-      </header>
-
-
-      <main>
-
-       <BrowserRouter>
-       <Routes>
-        <Route path="/" element={<Home/>}/>
-        <Route path="/about" element={<About/>}/>
-        <Route path="/contact" element={<Contact/>}/>
-       </Routes>
-       </BrowserRouter>
-      </main>
-
-    </div>
+    </BrowserRouter>
   );
 }
 
